9f88c41c91cef95cfa65d18110e156a87a0ffa35
arfani <arfanihidayat@gmail.com>
2018-07-26 15:49:55 +0800

    edit home lil

--- application/modules/home/views/content.php
+++ application/modules/home/views/content.php
@@ -1,4 +1,4 @@
-<div class="bg-light">
+<div class="bg-white">
   <!-- Carousel -->
 <div id="demo" class="carousel slide" data-ride="carousel">
 
@@ -36,20 +36,42 @@
 <div class="content">
   <div class="container">
     <div class="row">
-      <div class="col-sm-9">
-        <h1>Welcome to Jazz Senggigi Hotel Mataram</h1>
-        <p class="welcome-msg">
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
-          Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      <div class="col-sm-9 offset-sm-3">
+        <h1 class="title-right">Welcome to Jazz Senggigi Hotel Mataram</h1>
+        <p class="welcome-msg-justify">
+          Jazz senggigi Hotel di Lombok Nusa Tenggara Barat merupakan salah satu Hotel bintang 1 di Batu Bolong.
+          Senggigi yang mengusung konsep Standard & Budget Hotel, dengan luas tanah 1.276 m2 ( seribu dua ratus meter persegi ).
+          Berawal dari tujuan usaha yang memberikan akomodasi standard yang memberikan pelayanan yang istimewa untuk kepuasan setiap tamu, maka dibangunlah Jazz Senggigi Hotel dengan saran akomodasi & fasilitas yang memadai dengan harga yang ekonomis.
+          Jazz Senggigi Hotel mulai dibangun pada Tahun 2010 dengan owner pertama yang bernama LILIK HERAWATI dan sekarang sudah berganti Owner atas nama Abdullah Mohamad warga negara Malaysia yang tertarik untuk melakukan investasi di Lombok terutama di bidang perhotelan. Awal pembangunan Jazz Senggigi Hotel hanya mempunyai 8 Standard premium Room dilengkapi dengan 1 buah restoran untuk melayani tamu sarapan pagi/Breakfast. Setelah dibeli oleh pemilik yang baru dibangunlah kembali 5 kamar yang disebut sebagai BUDGET ROOM yang dikhususkan untuk tamu tamu Backpacker dari wisatawan asing maupun lokal yang mempunyai Low Budget. Seiring dengan berkembangnya pariwisata di Lombok khususnya Senggigi, Jazz Senggigi Hotel mulai menambah & membangun fasilitas pendukung untuk memberikan pelayanan yang baik kepada tamu dibuatlah swimmming pool/outdoor Pool lengkap dengan pool Bar agar tamu lebih merasa Relax dengan suasana & kenyamanan di Hotel.
         </p>
       </div>
     </div>
 
     <div class="row">
-      <div class="col-sm-9 offset-sm-3">
-          <h2 class="title-right">Our Services</h2>
-          <p class="welcome-msg-right">
-            Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.
+      <div class="col-sm-9">
+          <h2>Our Services</h2>
+            <ol>
+              <li>DJ Bar & Restaurant yang menyajikan menu mulai dari Asian,European & Western Food</li>
+              <li>ZZ Pool Bar tempat untuk bersantai dengan menu Cocktail dari Barman Hotel</li>
+              <li>Swimming Pool yang memiliki kedalaman yang berbeda yaitu 2.5meter</li>
+              <li>Laundry Service untuk tamu Hotel-surcharge</li>
+              <li>Motorbike & bicycle Rent</li>
+              <li>Tour program & Transport</li>
+              <li>Kids Pool</li>
+              <li>Locker</li>
+              <li>Guest Parking Area</li>
+              <li>Wifi di Semua Area Hotel</li>
+              <li>8 kamar Standard Premium</li>
+              <li>4 Budget Room</li>
+              <li>1 Dormitory Room dengan 4 Bed</li>
+            </ol>
+      </div>
+      <div class="col-sm-12">
+            <p class="welcome-msg">
+              <p>Soft Opening Jazz Senggigi Hotel dilaksanakan pada tanggal 25 Mei 2015 & Grand Opening dilakukan pada
+              Bulan September 2016 setelah fasilitas rampung untuk menunjang kepuasan para tamu yang tinggal di jazz
+              Senggigi Hotel, hingga saat ini jazz hotel sudah mempekerjakan kurang lebih 25 orang staff ditempatkan sesuai
+              dengan posisi & department yang ada. Visi Jazz Senggigi Hotel yaitu memberikan Service atau pelayanan yang memuaskan kepada Tamu serta memberikan pengalaman tinggal di Hotel yang mengusung konsep “FEELS LIKE HOME”.</p>
           </p>
       </div>
     </div>
